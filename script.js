3bf9930581e7d5cbea3aa38f1e772060ccecd5e1
satyam1024 <132131308+satyam1024@users.noreply.github.com>
2023-08-03 13:48:00 +0530

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-let vh = window.innerHeight * 0.01;
-let vw = window.innerWidth * 0.01;
-
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-document.documentElement.style.setProperty('--vw', `${vw}px`);
-
 
 
 var slideIndex = 1;
